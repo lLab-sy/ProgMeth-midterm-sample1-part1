64e8210705341f0fdbc7a0f407a42179700a2b08
Sarin Yongsuwan <yongsuwan.sb@gmail.com>
2023-03-05 17:58:09 +0700

    Modify class Order

--- src/logic/Order.java
+++ src/logic/Order.java
@@ -9,23 +9,37 @@ public class Order {
 	
 	public Order() {
 		// TODO
-
+		orderItemList = new ArrayList<OrderItem>();
+		orderNumber = totalOrderCount;
+		totalOrderCount += 1;
 	}
 
 	public OrderItem addItem(Item item, int amount) {
 		// TODO
+		for(OrderItem orderItem: orderItemList) {
+			if(orderItem.getItem().equals(item)) {
+				orderItem.increaseItemAmount(amount);
+				return orderItem;
+			}
+		}
+		OrderItem newOrder = new OrderItem(item, amount);
+		orderItemList.add(newOrder);
+		return newOrder;
 		// Loop the orderItemList to find if item of any orderItem equals to the given item
 		// if there is orderItem with given item, increase that orderItem itemAmount with amount
 		// and return that orderItem
 
 		// else create new orderItem with given item and amount, then return the new orderItem
-		return null;
 	}
 
 	public int calculateOrderTotalPrice() {
 		// TODO
 		// Calculate total price of the order by summing total price of each orderItem in orderItemList
-		
+		int total = 0;
+		for(OrderItem order: orderItemList) {
+			total += order.calculateTotalPrice();
+		}
+		return total;
 	}
 
 	public static int getTotalOrderCount() {
